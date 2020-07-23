ae15080e47d81b6e312b8aec13b49fa7663bcfab
Gustavo de Oliveira <gustavovital@id.uff.br>
2020-07-23 20:21:15 -0300

    add Gauthier Vermandel

---
+++ Gauthier Vermandel 2020/Chapter 01/+model/dynamic_g1_nz.m
@@ -0,0 +1,16 @@
+function [nzij_pred, nzij_current, nzij_fwrd] = dynamic_g1_nz()
+% Returns the coordinates of non-zero elements in the Jacobian, in column-major order, for each lead/lag (only for endogenous)
+  nzij_pred = zeros(2, 2, 'int32');
+  nzij_pred(1,1)=2; nzij_pred(1,2)=2;
+  nzij_pred(2,1)=3; nzij_pred(2,2)=3;
+  nzij_current = zeros(6, 2, 'int32');
+  nzij_current(1,1)=1; nzij_current(1,2)=1;
+  nzij_current(2,1)=2; nzij_current(2,2)=1;
+  nzij_current(3,1)=1; nzij_current(3,2)=2;
+  nzij_current(4,1)=2; nzij_current(4,2)=2;
+  nzij_current(5,1)=2; nzij_current(5,2)=3;
+  nzij_current(6,1)=3; nzij_current(6,2)=3;
+  nzij_fwrd = zeros(2, 2, 'int32');
+  nzij_fwrd(1,1)=1; nzij_fwrd(1,2)=1;
+  nzij_fwrd(2,1)=1; nzij_fwrd(2,2)=3;
+end
---
+++ Gauthier Vermandel 2020/Chapter 01/runModel.m
@@ -0,0 +1,8 @@
+%% Based on Gauthier Vermandel
+
+%% Start Dynare
+addpath C:\dynare\4.6.1\matlab
+
+%% Run Model
+clear; clc;
+dynare model
