36903110243457d40cd00952e88add4d753d11d1
Gustavo de Oliveira <gustavovital@id.uff.br>
2020-07-29 18:55:45 -0300

    comparation habit formation

--- Gauthier Vermandel 2020/Chapter 03/runModel.m
+++ Gauthier Vermandel 2020/Chapter 03/runModel.m
@@ -6,3 +6,51 @@ clear; clc;
 
 %% Run model
 dynare model
+
+irfs = oo_;
+save('irfs', 'irfs');
+%% Run model
+dynare model2
+irfs2 = oo_;
+save('irfs2', 'irfs2');
+
+%% Compare habit formation
+clear; clc;
+
+load('irfs');
+load('irfs2');
+
+%% Graphs
+figure;
+subplot(2,4,1)
+plot(irfs.irfs.y_n_c);
+hold on
+plot(irfs2.irfs.y_n_c);
+subplot(2,4,2)
+plot(irfs.irfs.c_n_c);
+hold on
+plot(irfs2.irfs.c_n_c);
+subplot(2,4,3)
+plot(irfs.irfs.i_n_c);
+hold on
+plot(irfs2.irfs.i_n_c);
+subplot(2,4,4)
+plot(irfs.irfs.k_n_c);
+hold on
+plot(irfs2.irfs.k_n_c);
+subplot(2,4,5)
+plot(irfs.irfs.r_n_c);
+hold on
+plot(irfs2.irfs.r_n_c);
+subplot(2,4,6)
+plot(irfs.irfs.w_n_c);
+hold on
+plot(irfs2.irfs.w_n_c);
+subplot(2,4,7)
+plot(irfs.irfs.h_n_c);
+hold on
+plot(irfs2.irfs.h_n_c);
+subplot(2,4,8)
+plot(irfs.irfs.z_n_c);
+hold on
+plot(irfs2.irfs.z_n_c);
